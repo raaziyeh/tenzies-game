989c4fe807a263d997d89ae9355e7492ec437983
raziyeh <rsoradimoghaddam@gmail.com>
2022-10-31 20:05:47 +0330

    Added the Tenzies game logic

--- src/App.js
+++ src/App.js
@@ -1,21 +1,89 @@
+// React
+import { useState, useEffect } from "react"
+// Packages
+import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 //Components
 import DicePlane from "./Components/Dice/DicePlane"
 import Button from "./Components/UI/Button"
-import classes from './App.module.scss';
+import classes from "./App.module.scss"
+
+function rollDie() {
+	return Math.floor(Math.random() * 6 + 1)
+}
+
+function initializeDice() {
+	let dice = []
+	for (let i = 0; i < 10; i++) {
+		dice.push({
+			id: nanoid(),
+			number: rollDie(),
+			isHeld: false,
+		})
+	}
+	return dice
+}
 
 function App() {
-  return (
+	const [hasWon, setHasWon] = useState(false)
+	const [dice, setDice] = useState(initializeDice)
+
+	const toggleDieHold = (id) => {
+		setDice(dice => {
+			return dice.map(die => {
+				if (die.id === id) {
+					return {
+						...die,
+						isHeld: !die.isHeld
+					}
+				}
+				return die				
+			})
+		})
+	}
+
+	const rollDice = () => {
+		setDice(dice => {
+			return dice.map(die => {
+				if (die.isHeld) {
+					return die
+				}
+				return {
+					...die,
+					number: rollDie()
+				}
+			})
+		})
+	}
+
+	const resetGame = () => {
+		setDice(initializeDice())
+		setHasWon(false)
+	}
+
+	useEffect(() => {
+		const isAllDiceIsHeld = dice.every(die => die.isHeld)
+		const isAllSameValue = dice.every(die => die.number === dice[0].number)
+		if (isAllDiceIsHeld && isAllSameValue) {
+			setHasWon(true)
+		}
+	}, [dice])
+
+
+	return (
 		<main className={classes.main}>
+			{hasWon && <Confetti />}
 			<h1 className={classes.header}>Tenzies</h1>
 			<p className={classes.description}>
 				Roll until all dice are the same. Click each die to freeze it at its
 				current value between rolls.
 			</p>
-			<DicePlane />
-			<Button onClick={() => console.log("roll")}>Roll</Button>
+			{hasWon && <p className={classes["wining-message"]}> Congratulations! You Won! 🎉</p>}
+			<DicePlane dice={dice} toggleDieHold={toggleDieHold} />
+			{!hasWon && <Button onClick={rollDice}>Roll</Button>}
+			{hasWon && <Button onClick={resetGame}> New Game</Button>}
 		</main>
-
 	)
 }
 
-export default App;
+export default App
--- src/Components/Dice/DicePlane.js
+++ src/Components/Dice/DicePlane.js
@@ -1,66 +1,13 @@
-import { useState } from "react"
 import Die from "./Die"
 import classes from "./DicePlane.module.scss"
 
-const DUMMY_DICE = [
-	{
-		id: 1,
-		number: 2,
-		isHeld: false,
-	},
-	{
-		id: 2,
-		number: 2,
-		isHeld: false,
-	},
-	{
-		id: 3,
-		number: 3,
-		isHeld: false,
-	},
-	{
-		id: 4,
-		number: 5,
-		isHeld: false,
-	},
-	{
-		id: 5,
-		number: 1,
-		isHeld: false,
-	},
-	{
-		id: 6,
-		number: 4,
-		isHeld: false,
-	},
-	{
-		id: 7,
-		number: 6,
-		isHeld: false,
-	},
-	{
-		id: 8,
-		number: 6,
-		isHeld: true,
-	},
-	{
-		id: 9,
-		number: 5,
-		isHeld: false,
-	},
-	{
-		id: 10,
-		number: 4,
-		isHeld: false,
-	},
-]
-function DicePlane() {
+function DicePlane(props) {
 	return (
 		<div className={classes.wrapper}>
 			<section className={classes.plane}>
-				{DUMMY_DICE.map((die) => {
+				{props.dice.map((die) => {
 					return (
-						<Die {...die} onClick={() => console.log("die id:" + die.id)} />
+						<Die {...die} key={die.id} onClick={() => props.toggleDieHold(die.id)} />
 					)
 				})}
 			</section>
